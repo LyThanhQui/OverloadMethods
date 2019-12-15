bafbfd33498d4c986c41e8bfbb7023c14d0f567e
Qui Ly <lquiitfuture@gmail.com>
2019-12-15 19:03:44 +0700

    change Program.cs file

--- Program.cs
+++ Program.cs
@@ -2,10 +2,36 @@
 
 namespace OverloadMethods
 {
-    class Program
+    /* class Program
+        {
+         //example 1
+         //overload methods in C#
+          public void add(int a, int b)
+          {
+              Console.WriteLine(a + b);
+          }
+          public void add(float x, float y)
+          {
+              Console.WriteLine(x + y);
+          }
+          public void add(string s1, string s2)
+          {
+              Console.WriteLine(s1 + s2);
+          }
+          static void Main(string[] args)
+          {
+              Program obj = new Program();
+              obj.add(10, 20);
+              obj.add(10.5f, 20.5f);
+              obj.add("pranaya", "kumar");
+              Console.WriteLine("Press any key to exist.");
+              Console.ReadKey();
+          }*/
+
+    //example 2
+    //inheritance based overloading
+    class ADD1
     {
-        //example 1
-        //overload methods in C#
         public void add(int a, int b)
         {
             Console.WriteLine(a + b);
@@ -14,19 +40,28 @@ namespace OverloadMethods
         {
             Console.WriteLine(x + y);
         }
+    }
+    class ADD2 : ADD1
+    {
         public void add(string s1, string s2)
         {
             Console.WriteLine(s1 + s2);
         }
+    }
+    class Program
+    {
         static void Main(string[] args)
         {
-            Program obj = new Program();
+            ADD2 obj = new ADD2();
             obj.add(10, 20);
             obj.add(10.5f, 20.5f);
             obj.add("pranaya", "kumar");
             Console.WriteLine("Press any key to exist.");
             Console.ReadKey();
         }
-
     }
+
+
+
+
 }
